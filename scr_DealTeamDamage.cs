4a0eade8605475b38f7feabac6e95372c2669181
Morgan Lee <mclee@fullsail.edu>
2016-11-05 15:53:09 -0400

    Code snippets

---
+++ scr_DealTeamDamage.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class scr_DealTeamDamage : MonoBehaviour
+{
+    // Used to determine the direction to damage the player
+    public enum Direction { North = 0, East, South, West, All, Unidentified };
+
+    // Used to keep track of the players positions
+    public GameObject[] players;
+
+    // Used to keep track of the players direction
+    scr_Player_TileBasedMovement playerDirection;
+
+    int randomPlayer;
+    int playersComparedDirection;
+    int enemiesComparedDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerDirection = gameObject.GetComponent<scr_Player_TileBasedMovement>();
+    }
+
+    //Ai reference
+    //(damage that will be dealt, direction enemy is facing, should damage be dealt to both, type of damage enemy is doing (Fire, Shadow, ect...))
+    public int TakeDamage(float dmg, Direction dir, bool dealDmgToBoth = false, scr_AIReferences.DamageTypes type = scr_AIReferences.DamageTypes.PHYSICAL)
+    {
+        if (dir != Direction.Unidentified)
+        {
+            //If the damage is not dealt to all team members
+            if (dir != Direction.All)
+            {
+                randomPlayer = Random.Range(0, 2);
+                playersComparedDirection = ((int)playerDirection.playerDirection) % 2;
+                enemiesComparedDirection = ((int)dir) % 2;
+
+                //If the Players compared direction is equal to the enemies compared direction 
+                //determine if the front row gets damage or the back row does;
+                if (playersComparedDirection == enemiesComparedDirection)
+                {
+                    //If the players direction is equal to the enemy direction then
+                    //deal damage to sub 2 or sub 3;
+                    if (((int)playerDirection.playerDirection) == (int)dir)
+                    {
+                        // Deal player damage to sub 3 or sub 2, or both
+                        if (!dealDmgToBoth)
+                        {
+                            if (randomPlayer == 0)
+                            {
+                                players[2].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 2;
+                            }
+                            else
+                            {
+                                players[3].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 3;
+                            }
+                        }
+                        else
+                        {
+                            players[2].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            players[3].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            return -1;
+                        }
+                    }
+                    else
+                    {
+                        // Deal damage to sub 0 or sub 1, or both
+                        if (!dealDmgToBoth)
+                        {
+                            if (randomPlayer == 0)
+                            {
+                                players[0].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 0;
+                            }
+                            else
+                            {
+                                players[1].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 1;
+                            }
+                        }
+                        else
+                        {
+                            players[0].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            players[1].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            return -1;
+                        }
+                    }
+                }
+                else
+                {
+                    enemiesComparedDirection = ((int)dir + 1) % 4;
+                    if (((int)playerDirection.playerDirection) == enemiesComparedDirection)
+                    {
+                        // Deal damage 0 or 3 or both
+                        if (!dealDmgToBoth)
+                        {
+                            if (randomPlayer == 0)
+                            {
+                                players[1].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 1;
+                            }
+                            else
+                            {
+                                players[2].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 2;
+                            }
+                        }
+                        else
+                        {
+                            players[1].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            players[2].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            return -1;
+                        }
+                    }
+                    else
+                    {
+                        // Deal damage sub 1 or 2 or both
+                        if (!dealDmgToBoth)
+                        {
+                            if (randomPlayer == 0)
+                            {
+                                players[0].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 0;
+                            }
+                            else
+                            {
+                                players[3].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                                return 3;
+                            }
+
+                        }
+                        else
+                        {
+                            players[0].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            players[3].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                            return -1;
+                        }
+                    }
+                }
+            }
+            //If damage is dealt to all team members
+            else
+            {
+                for (int i = 0; i < players.Length; i++)
+                {
+                    //If that team member is dead then continue to the next
+                    if (players[i].GetComponent<Scr_PlayerAttributes>().dead)
+                        continue;
+                    //Deal damage to each team member
+                    players[i].GetComponent<Scr_PlayerAttributes>().DamagePlayer(dmg, type);
+                }
+            }
+        }
+        return -1;
+    }
+}
+
---
+++ scr_MovingBloodTrail.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_MovingBloodTrail : MonoBehaviour
+{
+    public float copyTimer = 0.0f;                  // Copy of the timer for deug purposes
+    public float timer = 0.0f;                      
+    public float startBloodTimer = 0.1f;
+    public float offsetBloodChange = -0.6f;
+    public bool isMovingBlood = false;
+    public bool stopTimer = true;
+    public bool reset = false;
+    public GameObject bloodTrailTile;
+    AudioSource hauntedWalking;
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        hauntedWalking = gameObject.GetComponent<AudioSource>();
+        timer = 0.0f;
+        stopTimer = true;
+        copyTimer = startBloodTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 7.0f)
+        {
+            if (!isMovingBlood)
+            {
+                if (bloodTrailTile.GetComponent<scr_TileV2>().occupiedBy != null)
+                {
+                    if (bloodTrailTile.GetComponent<scr_TileV2>().occupiedBy.name == "Player")
+                    {
+                        stopTimer = false;
+                        isMovingBlood = true;
+                    }
+                } 
+            }
+
+            if (!stopTimer)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= startBloodTimer)
+                {
+                    gameObject.GetComponent<MeshRenderer>().enabled = true;
+                    hauntedWalking.Stop();
+                    hauntedWalking.Play();
+                    stopTimer = true;
+                }
+            }
+        }
+
+        if (reset)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            startBloodTimer = copyTimer;
+            isMovingBlood = false;
+            timer = 0.0f;
+            stopTimer = false;
+            reset = false;
+        }
+    }
+}
